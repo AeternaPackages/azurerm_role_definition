36b7903d787ab4f3df1d3ed418a183288a05d71f
Daniel Allen <dan@aeternagroup.net>
2026-07-08 23:41:16 +0100

    Update to schema version v4.80.0

---
+++ main.tf
@@ -0,0 +1,24 @@
+locals {
+  role_definitions = { for k1, v1 in var.role_definitions : k1 => { assignable_scopes = v1.assignable_scopes, description = v1.description, name = v1.name, permissions = v1.permissions, role_definition_id = v1.role_definition_id, scope = v1.scope } }
+
+  role_management_policies = merge([
+    for k1, v1 in var.role_definitions : {
+      for k2, v2 in coalesce(v1.role_management_policies, {}) :
+      "${k1}/${k2}" => merge(v2, {
+        role_definition_id = module.role_definitions.role_definitions["${k1}"].id
+      })
+    }
+  ]...)
+}
+
+module "role_definitions" {
+  source           = "git::https://github.com/AeternaModules/azurerm_role_definition.git?ref=v4.80.0"
+  role_definitions = local.role_definitions
+}
+
+module "role_management_policies" {
+  source                   = "git::https://github.com/AeternaModules/azurerm_role_management_policy.git?ref=v4.80.0"
+  role_management_policies = local.role_management_policies
+  depends_on               = [module.role_definitions]
+}
+
---
+++ variables.tf
@@ -0,0 +1,124 @@
+variable "role_definitions" {
+  description = <<EOT
+Map of role_definitions, attributes below
+Required:
+    - name
+    - scope
+Optional:
+    - assignable_scopes
+    - description
+    - role_definition_id
+    - permissions (block)
+Nested role_management_policies (azurerm_role_management_policy):
+    Required:
+        - scope
+    Optional:
+        - activation_rules (block)
+        - active_assignment_rules (block)
+        - eligible_assignment_rules (block)
+        - notification_rules (block)
+EOT
+
+  type = map(object({
+    name               = string
+    scope              = string
+    assignable_scopes  = optional(list(string))
+    description        = optional(string)
+    role_definition_id = optional(string)
+    permissions = optional(object({
+      actions          = optional(list(string))
+      data_actions     = optional(set(string))
+      not_actions      = optional(list(string))
+      not_data_actions = optional(set(string))
+    }))
+    role_management_policies = optional(map(object({
+      scope = string
+      activation_rules = optional(object({
+        approval_stage = optional(object({
+          primary_approver = list(object({
+            object_id = string
+            type      = string
+          }))
+        }))
+        maximum_duration                                   = optional(string)
+        require_approval                                   = optional(bool)
+        require_justification                              = optional(bool)
+        require_multifactor_authentication                 = optional(bool)
+        require_ticket_info                                = optional(bool)
+        required_conditional_access_authentication_context = optional(string)
+      }))
+      active_assignment_rules = optional(object({
+        expiration_required                = optional(bool)
+        expire_after                       = optional(string)
+        require_justification              = optional(bool)
+        require_multifactor_authentication = optional(bool)
+        require_ticket_info                = optional(bool)
+      }))
+      eligible_assignment_rules = optional(object({
+        expiration_required = optional(bool)
+        expire_after        = optional(string)
+      }))
+      notification_rules = optional(object({
+        active_assignments = optional(object({
+          admin_notifications = optional(object({
+            additional_recipients = optional(set(string))
+            default_recipients    = bool
+            notification_level    = string
+          }))
+          approver_notifications = optional(object({
+            additional_recipients = optional(set(string))
+            default_recipients    = bool
+            notification_level    = string
+          }))
+          assignee_notifications = optional(object({
+            additional_recipients = optional(set(string))
+            default_recipients    = bool
+            notification_level    = string
+          }))
+        }))
+        eligible_activations = optional(object({
+          admin_notifications = optional(object({
+            additional_recipients = optional(set(string))
+            default_recipients    = bool
+            notification_level    = string
+          }))
+          approver_notifications = optional(object({
+            additional_recipients = optional(set(string))
+            default_recipients    = bool
+            notification_level    = string
+          }))
+          assignee_notifications = optional(object({
+            additional_recipients = optional(set(string))
+            default_recipients    = bool
+            notification_level    = string
+          }))
+        }))
+        eligible_assignments = optional(object({
+          admin_notifications = optional(object({
+            additional_recipients = optional(set(string))
+            default_recipients    = bool
+            notification_level    = string
+          }))
+          approver_notifications = optional(object({
+            additional_recipients = optional(set(string))
+            default_recipients    = bool
+            notification_level    = string
+          }))
+          assignee_notifications = optional(object({
+            additional_recipients = optional(set(string))
+            default_recipients    = bool
+            notification_level    = string
+          }))
+        }))
+      }))
+    })))
+  }))
+
+  validation {
+    condition = alltrue(concat(
+      [for kk in keys(var.role_definitions) : !strcontains(kk, "/")],
+      flatten([for k0, v0 in var.role_definitions : [for kk in keys(coalesce(v0.role_management_policies, {})) : !strcontains(kk, "/")]])
+    ))
+    error_message = "Map keys in this package must not contain '/': it is used internally as a nesting-key separator, so a key containing it can silently collide two different nested entries into one. Rename the offending key(s)."
+  }
+}
